5da3aeced2b739e2a0a3f7d6f9cbc2c0e47cd2e6
Patrick <patrickpleumann@web.de>
2025-12-21 16:04:23 +0100

    humanoid rig now assigned to all animations

--- Assets/Scripts/Enemy_AI/EnemyStates/ShootState.cs
+++ Assets/Scripts/Enemy_AI/EnemyStates/ShootState.cs
@@ -1,12 +1,17 @@
+using UnityEditor;
 using UnityEngine;
 
 public class ShootState<T> : BaseState<T> where T : EnemyController
 {
     private bool canShoot = true;
     private float shootingTimer;
+    private float min;
+    private float max;
     public ShootState(T _controller) : base(_controller)
     {
         shootingTimer = 0f;
+        min = controller.SqrMinShootingDistance;
+        max = controller.SqrMaxShootingDistance;
     }
 
     public override BaseState<T> CheckConditions()
@@ -17,16 +22,21 @@ public class ShootState<T> : BaseState<T> where T : EnemyController
     public override void EnterState()
     {
         Debug.Log("Enter State: ShootState");
-        controller.agent.updatePosition = false; // could work maybe
+        //controller.agent.updatePosition = false; // could work maybe
         controller.agent.updateRotation = true;
-        controller.agent.speed = 1f; // just for turning the enemy while shooting
+        controller.agent.speed = 0.05f; // just for turning the enemy while shooting
     }
 
     public override void UpdateState()
     {
         //shoot
         shootingTimer -= Time.deltaTime;
-        controller.agent.destination = controller.player.position;
+        if (controller.SqrDistanceToPlayer > min && controller.SqrDistanceToPlayer < max)
+        {
+            controller.agent.destination = controller.player.position;
+            Debug.Log(controller.SqrDistanceToPlayer);
+        }
+
         if (canShoot == true && shootingTimer <= 0f && (controller.SqrDistanceToPlayer <= controller.SqrDesiredShootingRange))
         {
             controller.animator.SetTrigger("ShootAnim");
