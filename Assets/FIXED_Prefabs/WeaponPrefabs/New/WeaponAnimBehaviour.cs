9bf636e297a2a53e59750a3e151e644ad0313ad7
Patrick Pleumann <patrickpleumann@web.de>
2026-01-14 16:16:27 +0100

    working on animations

--- Assets/FIXED_Prefabs/WeaponPrefabs/New/WeaponAnimBehaviour.cs
+++ Assets/FIXED_Prefabs/WeaponPrefabs/New/WeaponAnimBehaviour.cs
@@ -3,16 +3,23 @@ using UnityEngine;
 
 public class WeaponAnimBehaviour : MonoBehaviour
 {
-    [SerializeField] private Animation shootAnim;
+    private Animation anim;
 
+
+    private void Awake()
+    {
+        anim = GetComponent<Animation>();
+    }
     private void Start()
     {
         StartCoroutine(Anim());
+        
+
     }
     private IEnumerator Anim()
     {
         yield return new WaitForSeconds(1);
-        shootAnim.Play();
+        anim.Play();
         yield break;
     }
 }
