15981d9f3d10c4a577e17384f59685f69367ef79
Patrick <patrickpleumann@web.de>
2026-01-22 21:48:36 +0100

    implemented logic for loading data into persistent data folder in runtime >> works good

--- Assets/Scripts/MenuScripts/ImportManager.cs
+++ Assets/Scripts/MenuScripts/ImportManager.cs
@@ -1,18 +1,22 @@
-using System;
-using UnityEditor;
+using SFB;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ImportManager : MonoBehaviour
 {
     //Load Data from file dialog into persitent data folder
+    private bool canChooseMultipleFiles;
 
     [SerializeField] private Button openFileDialog;
-    private const string message = "Browse Files";
+    [SerializeField] private string message;
+
+    private string destPath = null;
 
     private void Awake()
     {
-        
+        canChooseMultipleFiles = false;
+        destPath = Application.persistentDataPath + "/"; // + "/" is necessary to "hit" the correct folder
     }
 
     private void OnEnable()
@@ -22,6 +26,24 @@ public class ImportManager : MonoBehaviour
 
     private void BrowseFiles()
     {
-        var temp = EditorUtility.OpenFilePanel(message, null, ".wav");
+        //pauses mainthread, which is good
+        var temp = StandaloneFileBrowser.OpenFilePanel(message, "", "", canChooseMultipleFiles);  //stay with false to not 
+        if (temp.Length > 0)
+            LoadFileIntoPersistentDataFolder(temp[0]);
+    }
+
+    private void LoadFileIntoPersistentDataFolder(string _path) // whole logic for loading files into the persistent data folder
+    {
+        if (File.Exists(destPath + Path.GetFileName(_path)))
+            Debug.Log("File already exists in destination folder");
+
+        else if (File.Exists(_path) == true)
+        {
+            File.Copy(_path, destPath + Path.GetFileName(_path));
+            Debug.Log("File successfully loaded into persistent data folder");
+        }
+
+        else
+            Debug.Log("No file choosen");
     }
 }
