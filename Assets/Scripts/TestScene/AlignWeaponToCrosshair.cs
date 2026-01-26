81b1c1dc68df4a3359a320f0b895d4b570dd22ec
Patrick <patrickpleumann@web.de>
2026-01-26 20:44:57 +0100

    still needs polishing as before

--- Assets/Scripts/TestScene/AlignWeaponToCrosshair.cs
+++ Assets/Scripts/TestScene/AlignWeaponToCrosshair.cs
@@ -6,19 +6,17 @@ public class AlignWeaponToCrosshair : MonoBehaviour
     private Ray centerPoint;
     private RaycastHit hit;
     [SerializeField] private GameObject weapon;
-    [SerializeField] private LayerMask layerMask;
 
     private void FixedUpdate()
     {
-        AlignWeaponToCrosshairMiddlePoint();
+        AlignMuzzleToCrosshairMiddlePoint();
     }
-
     
 
-    private void AlignWeaponToCrosshairMiddlePoint()
+    private void AlignMuzzleToCrosshairMiddlePoint()
     {
         centerPoint = Camera.main.ViewportPointToRay(cameraCenterPoint);
-        var temp = Physics.Raycast(centerPoint, out hit, 100f, layerMask);
+        var temp = Physics.Raycast(centerPoint, out hit, 200f);
         if (temp == true)
         {
             weapon.transform.LookAt(hit.point);
