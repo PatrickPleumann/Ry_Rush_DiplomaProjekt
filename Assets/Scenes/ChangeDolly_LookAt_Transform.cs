043f72b95c07e6e24421e479853409b639c5469f
Patrick <patrickpleumann@web.de>
2026-02-18 13:37:01 +0100

    working on trailer dolly cam track

---
+++ Assets/Scenes/ChangeDolly_LookAt_Transform.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class ChangeDolly_LookAt_Transform : MonoBehaviour
+{
+    [SerializeField] public Transform newLookAtPos;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
+
